168ad08b0cd846b62c0cb267b7bc19900ff3a05a
FuXiaoHei <fuxiaohei@hexiaz.com>
2014-05-21 22:00:26 +0800

    add chunk map for cursor caching

--- chunk.go
+++ chunk.go
@@ -14,6 +14,8 @@ type Chunk struct {
 	directory     string
 	s             Mapper
 	size          int // size of data items in a chunk file
+	xFile         string
+	xMap          map[string]int
 }
 
 // get file name by cursor.
@@ -34,6 +36,23 @@ func (c *Chunk) getInRaw(key string) (cursor int, v interface{}) {
 	return
 }
 
+// get value by key in chunk.
+func (c *Chunk) getInChunk(key string, i int) (j int, v interface{}, e error) {
+	if len(c.raw[i]) < 1 {
+		e = c.readChunk(i)
+		if e != nil {
+			return
+		}
+	}
+	if _, ok := c.raw[i][key]; ok {
+		v = c.raw[i][key]
+		j = i
+		return
+	}
+	j = -1
+	return
+}
+
 // get value by key in prev chunks files.
 // The chunk files are not loaded in memory.
 func (c *Chunk) getInPrev(key string) (i int, v interface{}, e error) {
@@ -45,16 +64,17 @@ func (c *Chunk) getInPrev(key string) (i int, v interface{}, e error) {
 	}
 
 	// load chunk file
-	var tmp map[string]interface{}
-	e = c.s.FromFile(c.fileName(c.readC), &tmp)
+	e = c.readChunk(c.readC)
 	if e != nil {
 		return
 	}
-	c.raw[c.readC] = tmp
 
 	// find in this chunk data
-	if _, ok := tmp[key]; ok {
-		v = tmp[key]
+	i, v, e = c.getInChunk(key, c.readC)
+	if e != nil {
+		return
+	}
+	if v != nil {
 		return
 	}
 	return c.getInPrev(key)
@@ -63,6 +83,13 @@ func (c *Chunk) getInPrev(key string) (i int, v interface{}, e error) {
 // get value by key.
 // return the chunk cursor where value in and value data.
 func (c *Chunk) Get(key string) (i int, v interface{}, e error) {
+	i = c.xMap[key]
+	if i > 0 {
+		i, v, e = c.getInChunk(key, i)
+		if v != nil || e != nil {
+			return
+		}
+	}
 	i, v = c.getInRaw(key)
 	if i < 1 || v == nil {
 		i, v, e = c.getInPrev(key)
@@ -89,10 +116,13 @@ func (c *Chunk) Put(key string, v interface{}) (cursor int, e error) {
 		}
 		c.putInNext(key, v)
 		cursor = c.writeC
-		return
+	} else {
+		c.raw[c.writeC][key] = v
+		cursor = c.writeC
 	}
-	c.raw[c.writeC][key] = v
-	cursor = c.writeC
+
+	c.xMap[key] = cursor
+	e = c.flushXMap()
 	return
 }
 
@@ -108,6 +138,19 @@ func (c *Chunk) Del(cursor int, key string) {
 	if _, ok := c.raw[cursor]; ok {
 		delete(c.raw[cursor], key)
 	}
+	delete(c.xMap, key)
+}
+
+// read chunk by cursor int
+func (c *Chunk) readChunk(i int) (e error) {
+	// load chunk file
+	var tmp map[string]interface{}
+	e = c.s.FromFile(c.fileName(i), &tmp)
+	if e != nil {
+		return
+	}
+	c.raw[c.readC] = tmp
+	return
 }
 
 // flush chunk by cursor int
@@ -116,6 +159,18 @@ func (c *Chunk) flushChunk(i int) (e error) {
 		return
 	}
 	e = c.s.ToFile(c.fileName(i), c.raw[i])
+	return
+}
+
+// flush x map to file
+func (c *Chunk) flushXMap() (e error) {
+	e = c.s.ToFile(c.xFile, c.xMap)
+	return
+}
+
+// read x map from file
+func (c *Chunk) readXMap() (e error) {
+	e = c.s.FromFile(c.xFile, &c.xMap)
 	return
 }
 
@@ -128,6 +183,8 @@ func NewChunk(directory string, s Mapper, size int) (c *Chunk, e error) {
 	c.s = s
 	c.size = size
 	c.raw = make(map[int]map[string]interface{})
+	c.xFile = path.Join(directory, "datax.dat")
+	c.xMap = make(map[string]int)
 
 	// try to read chunk files
 	i := 2
@@ -145,6 +202,9 @@ func NewChunk(directory string, s Mapper, size int) (c *Chunk, e error) {
 		c.readC = 1
 		c.writeC = 1
 		c.raw[c.writeC] = make(map[string]interface{})
+		if e = c.flushXMap(); e != nil {
+			return
+		}
 		e = c.flushChunk(c.writeC)
 		return
 	}
@@ -152,11 +212,9 @@ func NewChunk(directory string, s Mapper, size int) (c *Chunk, e error) {
 	// read last chunk file to memory and save cursor
 	c.readC = i
 	c.writeC = i
-	var tmp map[string]interface{}
-	e = s.FromFile(c.fileName(c.readC), &tmp)
-	if e != nil {
+	if e = c.readXMap(); e != nil {
 		return
 	}
-	c.raw[c.readC] = tmp
+	e = c.readChunk(c.readC)
 	return
 }
